e0742d84731a6a2c95b306b22d215183e639bcd8
복준수 <brianbok97@gmail.com>
2025-06-25 22:04:01 +0900

    Add: 예비 yaml

--- dev/shell/create-message.sh
+++ dev/shell/create-message.sh
@@ -3,3 +3,8 @@ topic=$1
 /opt/kafka/bin/kafka-console-producer.sh \
   --topic "$topic" \
   --bootstrap-server kafka:29092
+
+#!/bin/bash
+/opt/kafka/bin/kafka-console-producer.sh \
+  --topic user-status \
+  --bootstrap-server kafka:29092
